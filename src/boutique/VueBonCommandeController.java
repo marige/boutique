23cceb69edbd9a7dd58649d378eb4b05ae820c7b
geres <geres@captaine-Vigs>
2017-07-27 13:27:13 +0100

    remove

--- src/boutique/VueBonCommandeController.java
+++ src/boutique/VueBonCommandeController.java
@@ -26,6 +26,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import jpaController.ArticleJpaController;
 import jpaControler.BonCommandeJpaController;
 import jpaControler.DetailBonCommandeJpaController;
 import jpaControler.FournisseurJpaController;
