4828f842943dfe60724b3f38275c00a05dcd8cb9
obamg <mokouin@yahoo.fr>
2017-08-04 21:04:01 +0300

    rapport de vente

--- src/report/REPORT.java
+++ src/report/REPORT.java
@@ -1,8 +1,6 @@
 package report;
 import java.sql.Connection;
 import java.util.HashMap;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
